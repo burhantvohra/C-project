4f1862d2b921a30287583d3854920adfb5ce3c0a
unknown <burhancampwala123@gmail.com>
2022-02-13 19:21:43 +0530

    c++

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int  y[10]={6,7,8,9,8,9,7,8,5,8};                                                                                                          
+    
+  printf("%d", y[4]);  
+return 0;
+}
